5e93a9602749a97f4cb206a9ed65c8ae5b634f98
jarin3956 <jarinmeethal@gmail.com>
2024-04-07 16:37:06 +0530

    service card updated

---
+++ src/Components/ServiceCard/ServiceCard.jsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import './ServiceCard.css';
+
+function ServiceCard() {
+    return (
+        <div className='serv-area'>
+            <div className='serv-container row justify-content-center justify-content-lg-between'>
+                <div className="col-12 col-lg-2 serv-cardarea">
+                    <div className="serv-card">
+                        <div className="serv-icon">
+                        <img src="/termlife.png" alt="Logo" className='w-100' />
+                        </div>
+                        <p className="serv-title">Term Life Insurance</p>
+                        <p className="serv-text">Explore new offers.</p>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-2 serv-cardarea">
+                    <div className="serv-card">
+                        <div className="serv-icon">
+                            <img src="/health.png" alt="Logo" className='w-100' />
+                        </div>
+                        <p className="serv-title">Health Insurance</p>
+                        <p className="serv-text">Explore new offers.</p>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-2 serv-cardarea">
+                    <div className="serv-card">
+                        <div className="serv-icon">
+                        <img src="/investment.webp" alt="Logo" className='w-100' />
+                        </div>
+                        <p className="serv-title">Investment Plans</p>
+                        <p className="serv-text">Explore new offers.</p>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-2 serv-cardarea">
+                    <div className="serv-card">
+                        <div className="serv-icon">
+                        <img src="/car.jpg" alt="Logo" className='w-100' />
+                        </div>
+                        <p className="serv-title">Car Insurance</p>
+                        <p className="serv-text">Explore new offers.</p>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-2 serv-cardarea">
+                    <div className="serv-card">
+                        <div className="serv-icon">
+                        <img src="/bike.png" alt="Logo" className='w-100' />
+                        </div>
+                        <p className="serv-title">2 Wheeler Insurance</p>
+                        <p className="serv-text">Explore new offers.</p>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-2 serv-cardarea">
+                    <div className="serv-card">
+                        <div className="serv-icon">
+                        <img src="/family.jpg" alt="Logo" className='w-100' />
+                        </div>
+                        <p className="serv-title">Family Health Insurance</p>
+                        <p className="serv-text">Explore new offers.</p>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-2 serv-cardarea">
+                    <div className="serv-card">
+                        <div className="serv-icon">
+                        <img src="/aeroplane.png" alt="Logo" className='w-100' />
+                        </div>
+                        <p className="serv-title">Travel Insurance</p>
+                        <p className="serv-text">Explore new offers.</p>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-2 serv-cardarea">
+                    <div className="serv-card">
+                        <div className="serv-icon">
+                        <img src="/term-women.webp" alt="Logo" className='w-100' />
+                        </div>
+                        <p className="serv-title">Term Insurance Women</p>
+                        <p className="serv-text">Explore new offers.</p>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-2 serv-cardarea">
+                    <div className="serv-card">
+                        <div className="serv-icon">
+                        <img src="/child-savings.jpg" alt="Logo" className='w-100' />
+                        </div>
+                        <p className="serv-title">Child Savings Plans</p>
+                        <p className="serv-text">Explore new offers.</p>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-2 serv-cardarea">
+                    <div className="serv-card">
+                        <div className="serv-icon">
+                        <img src="/retirement.jpg" alt="Logo" className='w-100' />
+                        </div>
+                        <p className="serv-title">Retirement Plans</p>
+                        <p className="serv-text">Explore new offers.</p>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-2 serv-cardarea">
+                    <div className="serv-card">
+                        <div className="serv-icon">
+                        <img src="/employee.jpg" alt="Logo" className='w-100' />
+                        </div>
+                        <p className="serv-title">Employe Insurance</p>
+                        <p className="serv-text">Explore new offers.</p>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-2 serv-cardarea">
+                    <div className="serv-card">
+                        <div className="serv-icon">
+                        <img src="/home.jpg" alt="Logo" className='w-100' />
+                        </div>
+                        <p className="serv-title">Home Insurance</p>
+                        <p className="serv-text">Explore new offers.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ServiceCard
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -5,12 +5,14 @@ import Footer from '../Components/Footer/Footer';
 import PartnerCard from '../Components/PartnersCard/PartnerCard';
 import AdvantageCard from '../Components/AdvantageCard/AdvantageCard';
 import AchievementCard from '../Components/AchievementCard/AchievementCard';
+import ServiceCard from '../Components/ServiceCard/ServiceCard';
 
 function LandingPage() {
   return (
     <>
     <Navbar/>
     <BannerCard/>
+    <ServiceCard/>
     <AchievementCard/>
     <AdvantageCard/>
     <PartnerCard/>
